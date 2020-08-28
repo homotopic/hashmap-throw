cab2d64b2d012f097428084c8e10d1f5c2e3bccb
Dan Firth <locallycompact@gmail.com>
2020-08-28 16:02:57 +0100

    Add docstrings

--- src/Data/HashMap/Throw.hs
+++ src/Data/HashMap/Throw.hs
@@ -16,11 +16,13 @@ import Data.Typeable
 import Data.Hashable
 import Data.HashMap as HM
 
+-- | Exception thrown when a key is not found in a hashmap.
 newtype KeyNotFoundException a = KeyNotFoundException a
   deriving (Eq, Ord, Show)
 
 instance (Typeable a, Show a) => Exception (KeyNotFoundException a)
 
+-- | `HM.lookup` lifted to `MonadThrow` that throws `KeyNotFoundException`.
 lookupOrThrow :: (Eq a, Ord a, Show a, Typeable a, Hashable a, MonadThrow m) => a -> HashMap a b -> m b
 lookupOrThrow k m = case HM.lookup k m of
                           Just x -> return x
